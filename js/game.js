df6f2a9f41327d5f5d77d5616fe9a6b5850cafbd
tyler dahlgren <dahlgren15@gmail.com>
2021-03-12 11:57:09 -0800

    managed to add the background and the origin point

--- js/game.js
+++ js/game.js
@@ -1,6 +1,20 @@
 //create new scene
 let gameScene = new Phaser.Scene('Game');
 
+gameScene.preload = function(){
+    //load background
+    this.load.image('background', 'assets/background.png');
+    this.load.image('player', 'assets/player.png');
+};
+
+//called after preload
+gameScene.create = function(){
+    let background = this.add.sprite(0,0, 'background');//2 dimensional coordinate system...origin is in the top left corner of the game. 
+    background.setOrigin(0,0);
+}
+
+
+
 // set the configuration of the gaem
 let config = {
     type: Phaser.AUTO, //phaser will use WebGL or CANVAS
