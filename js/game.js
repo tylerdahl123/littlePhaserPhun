d4e7c70c420c3cf032fe6951b567e3969b2b8176
tyler dahlgren <dahlgren15@gmail.com>
2021-03-15 15:33:32 -0700

    added some little dragons and scaled them...no pun intended

--- js/game.js
+++ js/game.js
@@ -5,6 +5,7 @@ gameScene.preload = function(){
     //load background
     this.load.image('background', 'assets/background.png');
     this.load.image('player', 'assets/player.png');
+    this.load.image('enemy', "assets/dragon.png");
 };
 
 //called after preload
@@ -12,6 +13,19 @@ gameScene.create = function(){
     let background = this.add.sprite(0,0, 'background');//2 dimensional coordinate system...origin is in the top left corner of the game. 
     background.setOrigin(0,0);
     let player = this.add.sprite(70,180, 'player');
+    player.depth = 1; 
+    player.setScale(1);
+    let enemy1 = this.add.sprite(250, 180, 'enemy');
+    enemy1.setScale(0.5);
+
+    let enemy2 = this.add.sprite(450, 180, 'enemy');
+    enemy2.setScale(0.5);
+    //depth allows things to be pulled up or down in the rendering order
+    //to get sprite scale you have to look at the properties in the image through windows explorer
+    // player.x = 10;
+    // player.y = 
+    //sprites are shown in the order they are listed
+    //player is on top of the background
     // background.setPosition(640/2, 360/2);
 }
 
